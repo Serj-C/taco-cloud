f16b7949bc160e2d062a35a52f4218e90e504f75
Serj C <serj.cibotarica@gmail.com>
2022-11-02 21:12:12 +0200

    Added form input validation.

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -3,11 +3,13 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -47,7 +49,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco) {
+    public String processTaco(@Valid @ModelAttribute("taco") Taco taco, Errors errors) {
+        if (errors.hasErrors()) {
+           return "design";
+        }
+
+        //TODO: Save the taco
         log.info("Processing taco: " + taco);
 
         return "redirect:/orders/current";
